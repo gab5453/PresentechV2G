6aa7096d6d812f2e3dbd11caca602e8fe4637925
Gabriel Vasconez <gabrielaveiga28092005@gmail.com>
2026-06-18 22:41:03 -0500

    Datos semilla

--- src/backend/Presentech.DataAccess/Migrations/20260619034044_SeedDiasSemana.Designer.cs
+++ src/backend/Presentech.DataAccess/Migrations/20260619034044_SeedDiasSemana.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Presentech.DataAccess.Context;
@@ -11,9 +12,11 @@ using Presentech.DataAccess.Context;
 namespace Presentech.DataAccess.Migrations
 {
     [DbContext(typeof(PresentechDbContext))]
-    partial class PresentechDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260619034044_SeedDiasSemana")]
+    partial class SeedDiasSemana
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/backend/Presentech.DataAccess/Migrations/20260619034044_SeedDiasSemana.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Presentech.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDiasSemana : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "dias_semana",
+                columns: new[] { "id_dia", "nombre", "orden" },
+                values: new object[,]
+                {
+                    { 1, "Lunes", 1 },
+                    { 2, "Martes", 2 },
+                    { 3, "Miércoles", 3 },
+                    { 4, "Jueves", 4 },
+                    { 5, "Viernes", 5 },
+                    { 6, "Sábado", 6 },
+                    { 7, "Domingo", 7 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "dias_semana",
+                keyColumn: "id_dia",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "dias_semana",
+                keyColumn: "id_dia",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "dias_semana",
+                keyColumn: "id_dia",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "dias_semana",
+                keyColumn: "id_dia",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "dias_semana",
+                keyColumn: "id_dia",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "dias_semana",
+                keyColumn: "id_dia",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "dias_semana",
+                keyColumn: "id_dia",
+                keyValue: 7);
+        }
+    }
+}
